98387d0006e712762e0c025335447fad7893fa4b
Kevin Cahuana <kevin.cahuana.c@uni.pe>
2020-04-29 01:14:37 -0500

    access cors

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Reescribir rutas
 app.use('/api', user_routes);
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/api-rest-node', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
+
         console.log("La coneccion a Mongo DB es correcta");
         app.listen(port, () => {
             console.log("El servidor empezo a correr correctamente");
