f0a5385b94a6ae3aacaf6ded2db5e6d0ba6805d9
Kevin Cahuana <kevin.cahuana.c@uni.pe>
2020-05-04 18:11:54 -0500

    config

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.port || 3999;
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const DB_URI = process.env.DB_URI;
+const PORT = process.env.PORT;
 
 //Desabilitando un antiguo metodo que genera warning
 mongoose.set('useFindAndModify', false);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://cahuana:shinigami123x00@cluster0-rjljq.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
 
         console.log("La coneccion a Mongo DB es correcta");
-        app.listen(port, () => {
+        app.listen(PORT, () => {
             console.log("El servidor empezo a correr correctamente");
         });
     })
